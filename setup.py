8096483a7e3cade0e09fb50925ce5085cb14c471
Scolymus <20840716+Scolymus@users.noreply.github.com>
2022-06-30 23:33:24 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='proxy_checker',
-    version='0.6',
+    version='0.7',
     packages=['proxy_checker'],
     install_requires=['pycurl'],
     author='ricerati',
